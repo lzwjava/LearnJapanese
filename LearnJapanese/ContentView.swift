972fe55e86f54d337ad7ed3e60d56ad627f9d779
lzwjava <lzwjava@gmail.com>
2021-02-16 02:09:24 +0800

    add

--- LearnJapanese/ContentView.swift
+++ LearnJapanese/ContentView.swift
@@ -28,14 +28,44 @@ struct ContentView: View {
     
     func paintI(path: inout Path) {
         path.move(to: CGPoint(x: 0, y: 0))
-        path.addCurve(to: CGPoint(x: 40, y: 80), control1: CGPoint(x: 0, y: 120), control2: CGPoint(x: 20, y: 120))
+        path.addCurve(to: CGPoint(x: 35, y: 60), control1: CGPoint(x: 0, y: 90), control2: CGPoint(x: 20, y: 90))
+        
+        path.move(to: CGPoint(x: 50, y: 10))
+        path.addCurve(to: CGPoint(x: 65, y: 60), control1: CGPoint(x: 60, y: 35), control2: CGPoint(x: 60, y: 35))
+    }
+    
+    func paintU(path: inout Path) {
+        path.move(to: CGPoint(x: 8, y: 0))
+        path.addLine(to: CGPoint(x: 50, y: 5))
+        
+        path.move(to: CGPoint(x: 0, y: 30))
+        path.addCurve(to: CGPoint(x: 10, y: 100), control1: CGPoint(x: 80, y: 0), control2: CGPoint(x: 100, y: 70))
+    }
+    
+    func paintE(path: inout Path) {
+        
+    }
+    
+    func paintKu(path: inout Path) {
+        path.move(to: CGPoint(x: 80, y: 5))
+        path.addLine(to: CGPoint(x: 20, y: 45))
+        path.addLine(to: CGPoint(x: 80, y: 95))
+    }
+    
+    func paintHe(path: inout Path) {
+        path.move(to: CGPoint(x: 10, y: 60))
+        path.addLine(to: CGPoint(x: 35, y: 40))
+        path.addLine(to: CGPoint(x: 80, y: 70))
     }
     
     var body: some View {
         Path { path in
             
-            paintYa(path: &path)
+//            paintYa(path: &path)
 //            paintI(path: &path)
+//            paintU(path: &path)
+//            paintKu(path: &path)
+//            paintHe(path: &path)            
         }.stroke(Color.blue, lineWidth: 8).padding()
     }
 }
